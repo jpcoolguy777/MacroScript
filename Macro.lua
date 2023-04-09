a3354eb50cf87d1df3e36f8bcaf7c70f90882e66
jpcoolguy777 <123331288+jpcoolguy777@users.noreply.github.com>
2023-04-09 15:53:57 +0200

    Update Macro.lua

--- Macro.lua
+++ Macro.lua
@@ -1,18 +1,10 @@
-print("Made by JP")
 
-power = 3000 -- change this to make it more or less powerful
- 
-game:GetService('RunService').Stepped:connect(function()
-game.Players.LocalPlayer.Character.Head.CanCollide = false
-game.Players.LocalPlayer.Character.UpperTorso.CanCollide = false
-game.Players.LocalPlayer.Character.LowerTorso.CanCollide = false
-game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = false
-end)
-wait(.1)
-local bambam = Instance.new("BodyThrust")
-bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
-bambam.Force = Vector3.new(power,0,power)
-bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 
+while true do
 game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = 500
+ 
+
+wait()
+end 
+
