9131588dcf907debc7b0e3f8905322a49d0e4225
jpcoolguy777 <123331288+jpcoolguy777@users.noreply.github.com>
2023-04-09 16:01:06 +0200

    Update Macro.lua

--- Macro.lua
+++ Macro.lua
@@ -1,10 +1,7 @@
-
-
 while true do
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 1000000
 game.Players.LocalPlayer.Character.Humanoid.JumpPower = 500
  
 
 wait()
 end 
-
